f82f60cf15cfdffc5511170f8fc8fc8ace612678
energizet <a2437@yandex.ru>
2025-09-27 20:16:11 +0300

    add route

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,14 +1,68 @@
-import { ConstructorPage } from '@pages';
 import '../../index.css';
-import styles from './app.module.css';
-
-import { AppHeader } from '@components';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
+import { Layout } from './layout';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
+import { Modal } from '../modal';
+import { OrderInfo } from '../order-info';
+import { IngredientDetails } from '../ingredient-details';
 
 const App = () => (
-  <div className={styles.app}>
-    <AppHeader />
-    <ConstructorPage />
-  </div>
+  <RouterProvider
+    router={createBrowserRouter(
+      createRoutesFromElements(
+        <Route element={<Layout />}>
+          <Route path='/' element={<ConstructorPage />} />
+          <Route path='/feed' element={<Feed />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/reset-password' element={<ResetPassword />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile/orders' element={<ProfileOrders />} />
+          <Route path='*' element={<NotFound404 />} />
+          <Route
+            path='/feed/:number'
+            element={
+              <Modal title='OrderInfo' onClose={() => {}}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+          <Route
+            path='/ingredients/:id'
+            element={
+              <Modal title='IngredientDetails' onClose={() => {}}>
+                <IngredientDetails />
+              </Modal>
+            }
+          />
+          <Route
+            path='/profile/orders/:number'
+            element={
+              <Modal title='OrderInfo' onClose={() => {}}>
+                <OrderInfo />
+              </Modal>
+            }
+          />
+        </Route>
+      )
+    )}
+  />
 );
 
 export default App;
